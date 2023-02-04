6fbe2dad89997449a08a009cc7d0080a7c97f27c
Seongbin Kim <seongbin9786@gmail.com>
2023-02-04 21:04:06 +0900

    [#10, #11, #12] Todo 아이템 수정 모드, 삭제 기능 구현 (#14)

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,23 +1,72 @@
+import { useState } from "react";
+
 interface TodoListItemProps {
   name: string;
   checked: boolean;
   handleCheck: () => void;
+  handleSubmit: (name: string) => void;
+  handleDelete: () => void;
 }
 
 export function TodoListItem({
   name,
   checked,
   handleCheck,
+  handleSubmit,
+  handleDelete,
 }: TodoListItemProps) {
+  const [modifyMode, setModifyMode] = useState(false);
+  const [input, setInput] = useState(name);
+  const commitModified = async () => {
+    await handleSubmit(input);
+    setModifyMode(false);
+  };
+
   return (
-    <label style={{ display: "block" }}>
-      <input
-        type="checkbox"
-        name="checked"
-        defaultChecked={checked}
-        onChange={handleCheck}
-      />
-      <span>{name}</span>
-    </label>
+    <div style={{ display: "flex", gap: 16, height: 48, alignItems: "center" }}>
+      <label style={{ width: 240, display: "flex", gap: 4 }}>
+        <input
+          type="checkbox"
+          name="checked"
+          defaultChecked={checked}
+          onChange={handleCheck}
+        />
+        {modifyMode ? (
+          <input
+            data-testid="modify-input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        ) : (
+          <span>{name}</span>
+        )}
+      </label>
+
+      {modifyMode ? (
+        <>
+          <button data-testid="submit-button" onClick={commitModified}>
+            제출
+          </button>
+          <button
+            data-testid="cancel-button"
+            onClick={() => setModifyMode(false)}
+          >
+            취소
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            data-testid="modify-button"
+            onClick={() => setModifyMode(true)}
+          >
+            수정
+          </button>
+          <button data-testid="delete-button" onClick={handleDelete}>
+            삭제
+          </button>
+        </>
+      )}
+    </div>
   );
 }
