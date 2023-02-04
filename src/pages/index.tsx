380bbf35a59e3ed40aac9505d4c99f269242dba5
Seongbin Kim <seongbin9786@gmail.com>
2023-02-04 15:33:37 +0900

    feat(TodoList): Added loggedIn state to avoid Unauthorized state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 import { SignInPage } from "./SignInpage";
 import { SignUpPage } from "./SignUpPage";
+import { TodoListPage } from "./TodoListPage";
 
 export const RootRouter = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
   const [accessToken, setAccessToken] = useState("");
+  const updateAccessToken = (newAccessToken: string) => {
+    setAccessToken(newAccessToken);
+    setLoggedIn(true);
+  };
+  const logout = () => {
+    setAccessToken("");
+    setLoggedIn(false);
+    localStorage.removeItem("accessToken");
+  };
+
+  useEffect(() => {
+    const fromLocalStorage = localStorage.getItem("accessToken");
+    if (fromLocalStorage) {
+      updateAccessToken(fromLocalStorage);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -12,17 +31,17 @@ export const RootRouter = () => {
           path="/"
           element={
             <div>
-              <h1>Under App.tsx</h1>
-              <p>current in-memory AccessToken: [{accessToken}]</p>
-              <p>
-                current localstorage AccessToken: [
-                {localStorage.getItem("accessToken")}]
-              </p>
+              <h1>Welcome to TodoList App</h1>
+              <p>current AccessToken: [{accessToken}]</p>
+              {loggedIn && <button onClick={logout}>Logout</button>}
+              <br />
               <Link to="/">Move to main</Link>
               <br />
               <Link to="/signin">Move to Sign in</Link>
               <br />
               <Link to="/signup">Move to Sign up</Link>
+              <br />
+              <Link to="/todo">Move to Todo List</Link>
               <Outlet />
             </div>
           }
@@ -37,21 +56,23 @@ export const RootRouter = () => {
           />
           <Route
             path="signin"
-            element={<SignInPage setAccessToken={setAccessToken} />}
+            element={<SignInPage setAccessToken={updateAccessToken} />}
           />
           <Route
             path="signup"
-            element={<SignUpPage setAccessToken={setAccessToken} />}
+            element={<SignUpPage setAccessToken={updateAccessToken} />}
           />
-
-          {/* Using path="*"" means "match anything", so this route
-        acts like a catch-all for URLs that we don't have explicit
-        routes for. */}
+          <Route
+            path="todo"
+            element={
+              <TodoListPage loggedIn={loggedIn} accessToken={accessToken} />
+            }
+          />
           <Route
             path="*"
             element={
               <div>
-                <h1>404 Not found</h1>
+                <h1>Page Not found</h1>
               </div>
             }
           />
