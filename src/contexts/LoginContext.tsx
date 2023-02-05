579e7f45f959e287323752d22aa9b3a7f627aff3
Seongbin Kim <seongbin9786@gmail.com>
2023-02-05 16:39:09 +0900

    refactor(Login Context): Created LoginContext

---
+++ src/contexts/LoginContext.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+const ACCESS_TOKEN = 'access_token';
+
+const initialState = {
+  loggedIn: false,
+  accessToken: '',
+  // eslint-disable-next-line
+  login: (newAccessToken: string) => {},
+  // eslint-disable-next-line
+  logout: () => {},
+};
+
+export const LoginContext = createContext(initialState);
+
+export const LoginProvider = ({ children }: React.PropsWithChildren) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [accessToken, setAccessToken] = useState('');
+
+  const login = (newAccessToken: string) => {
+    localStorage.setItem(ACCESS_TOKEN, accessToken);
+    setAccessToken(newAccessToken);
+    setLoggedIn(true);
+  };
+
+  const logout = () => {
+    setAccessToken('');
+    setLoggedIn(false);
+    localStorage.removeItem(ACCESS_TOKEN);
+  };
+
+  const loadAccessToken = () => {
+    const fromLocalStorage = localStorage.getItem(ACCESS_TOKEN);
+    if (fromLocalStorage) {
+      login(fromLocalStorage);
+    }
+  };
+
+  useEffect(() => {
+    loadAccessToken();
+  }, []);
+
+  return (
+    <LoginContext.Provider
+      value={{
+        loggedIn,
+        accessToken,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+};
